4509e0b45dcd04f1b29e53ae737e4b5d22c34693
Shurc1val <hughpm16@gmail.com>
2023-12-08 13:49:16 +0000

    much cleaner

--- day_8.py
+++ day_8.py
@@ -6,8 +6,9 @@ from read_data_function import read_data
 # ----- PART ONE -----
 
 
-def traverse_nodes(current: str, destination: str, directions: list[str], node_dict: dict):
+def traverse_nodes(start: str, destination: str, directions: list[str], node_dict: dict):
     count = 0
+    current = start
     while current != destination:
         count += 1
         direction = directions[(count - 1)%len(directions)]
@@ -60,35 +61,6 @@ def z_list_for_node(node: str, directions: list[str], node_dict:dict) -> list[st
     return z_list
 
 
-def loop_rate_for_node(z_node: str, directions: list[str], node_dict:dict) -> int:
-    count = 0
-    current = z_node
-    print(directions)
-    int_directions = list(map(lambda x: 0 if x == "L" else 1, directions))
-    while (current != z_node) or (count == 0):
-        count += 1
-        direction = int_directions.pop(0)
-        int_directions.append(direction)
-        current = node_dict[current][direction]
-    return count
-
-
-def return_rate(node: str, directions: list[str], node_dict: dict) -> int:
-    return traverse_nodes(node, node, directions, node_dict)
-
-
-def traverse_nodes_v2(currents: list[str], directions: list[str], node_dict: dict):
-    count = 0
-    int_directions = list(map(lambda x: 0 if x == "L" else 1, directions))
-    while any([current[-1] != "Z" for current in currents]):
-        count += 1
-        direction = int_directions.pop(0)
-        int_directions.append(direction)
-
-        currents = list(map(lambda x: node_dict[x][direction], currents))
-    return count
-
-
 def part_two(str_input: str):
     directions = list(str_input.split('\n\n')[0])
     nodes = str_input.split('\n\n')[1].split('\n')
@@ -106,7 +78,7 @@ def part_two(str_input: str):
         print(len(directions))
         factors_and_constants.append({
             'constant': constant,
-            'factor': loop_rate_for_node(z_node, directions[constant:] + directions[:constant], node_dict)
+            'factor': traverse_nodes(z_node, z_node, directions[constant:] + directions[:constant], node_dict)
         })
     #factor always == constant, for some reason, and is always a divisor of directions, which makes it much easier
     return np.lcm.reduce([node['factor'] for node in factors_and_constants])
