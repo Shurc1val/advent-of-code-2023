f1b86c79602790acc19f1946d6c7e2bfbe38d549
Shurc1val <hughpm16@gmail.com>
2023-12-13 13:33:10 +0000

    still having some issues with all q mark rows

--- day_12.py
+++ day_12.py
@@ -19,6 +19,16 @@ def check_partial_combination(row: str, spring_strings: list[str], combination: 
     exp_row_length = len(experiment_row)
     if not check_row_valid(row[:exp_row_length], experiment_row):
         return False
+    
+    if len(row[exp_row_length:]) < sum([len(spring_string) for spring_string in spring_strings[len(combination):]]):
+        return False
+
+    if row[exp_row_length:].count('?') + row[exp_row_length:].count('#') < sum([len(spring_string) - 1 for spring_string in spring_strings[len(combination):]]):
+        return False
+    
+    if row[exp_row_length:].count('?') + row[exp_row_length:].count('.') < len(spring_strings) - len(combination):
+        return False
+    
     return True
 
 
@@ -88,7 +98,6 @@ def get_num_combinations_for_row(row: str, numbers: list[int], cache_dict: dict 
         spring_strings = [f"{'#'*number}." for number in numbers]
         num_spaces_to_fill = len(row) - len(''.join(spring_strings))
         if num_spaces_to_fill and spring_strings:
-            
             possible_space_combinations = get_possible_space_combinations_validate(num_spaces_to_fill, len(spring_strings) + 1, row, spring_strings)
             num_correct_combos = 0
             for combination in possible_space_combinations:
@@ -118,6 +127,7 @@ def test_part_one():
     assert part_one('?###???????? 3,2,1') == 10
     assert part_one('.??..??...?##. 1,1,3') == 4
     assert part_one('?#?#?#?#?#?#?#? 1,3,1,6') == 1
+    assert part_one('?.???.???? 1,1,1') == 29
     assert part_one("""???.### 1,1,3
 .??..??...?##. 1,1,3
 ?#?#?#?#?#?#?#? 1,3,1,6
@@ -129,6 +139,7 @@ def test_part_one():
 
 # ----- PART TWO -----
 
+from datetime import datetime
 
 def part_two(str_input: str):
     lines = str_input.split('\n')
@@ -136,9 +147,14 @@ def part_two(str_input: str):
     spring_numbers = [[int(num) for num in line.split()[1].split(',')]*5 for line in lines]
     sum = 0
     for i, inputs in enumerate(zip(spring_rows, spring_numbers)):
-        print(i, inputs[0],inputs[1])
-        num_comb = get_num_combinations_for_row(inputs[0],inputs[1])
-        sum += num_comb
+        if inputs[0].count('#') == 0:
+            print(datetime.time(datetime.now()))
+            print(i, inputs[0],inputs[1])
+            sum += get_num_combinations_for_row(inputs[0],inputs[1])
+            print(datetime.time(datetime.now()))
+        else:
+            print(i)
+            sum += get_num_combinations_for_row(inputs[0],inputs[1])
     return sum
 
 
@@ -159,5 +175,6 @@ def test_part_two():
 
 
 if __name__ == "__main__":
-    print(part_one('?#?#?#?#?#?#? 1,3,1,6'))
-    print(part_two(read_data("day_12.txt")))
+    print(part_one('?.???.???? 1,1,1'))
+    print(part_two('?.???.???? 1,1,1'))
+    #print(part_two(read_data("day_12.txt")))
