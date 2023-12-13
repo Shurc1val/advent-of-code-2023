91916f9ddbf5da95fb915446619959f6ccf8f293
Shurc1val <hughpm16@gmail.com>
2023-12-13 17:37:42 +0000

    blimey - day 12, prt 2 solved

--- day_12.py
+++ day_12.py
@@ -8,6 +8,9 @@ from read_data_function import read_data
 
 
 def check_row_valid(row: str, experiment_row: str):
+    if len(row) != len(experiment_row):
+        return False
+    
     for i in range(len(row)):
         if (row[i] != '?') and (row[i] != experiment_row[i]):
             return False
@@ -65,22 +68,70 @@ def get_first_row_spring_string(row: str) -> list[dict]:
     return None
 
 
+def get_number_substring_arrangements(string_length: int, substring_lengths: list[int], cache_dict: dict = None):
+    """NOTE - substring lengths are ordered"""
+    if cache_dict is None:
+        cache_dict = {}
+    if sum(substring_lengths) > string_length:
+        return 0
+    
+    if (sum(substring_lengths) == string_length) or (substring_lengths == []):
+        return 1
+    
+    num_arrangements = 0
+    for i in range(0, string_length - substring_lengths[0] + 1):
+        num = cache_dict.get(f'{i} {str(substring_lengths[1:])}', None)
+        if num is None:
+            num = get_number_substring_arrangements(i, substring_lengths[1:], cache_dict)
+            cache_dict[f'{i} {str(substring_lengths[1:])}'] = num
+        num_arrangements += num
+    
+    return num_arrangements
+
+
+
+def possible_no_hash_options(row: int, numbers_alt: list[int], cache_dict: dict = None):
+    if cache_dict is None:
+        cache_dict = {}
+
+    if sum(numbers_alt) > len(row):
+        return 0
+    
+    if row.count('.') > 0:
+        num_correct_combos = 0
+        sub_row = len(row.split('.', 1)[0]) + 1
+        for i in range(0, len(numbers_alt) + 1):
+                if sum(numbers_alt[:i]) <= sub_row and sum(numbers_alt[i:]) <= len(row) - sub_row:
+                    rem_row = row.split('.', 1)[1]
+                    rem_nums = numbers_alt[i:]
+                    rem_options = cache_dict.get(f'{str(rem_row)} {str(rem_nums)}', None)
+                    if rem_options is None:
+                        rem_options = possible_no_hash_options(rem_row, rem_nums, cache_dict)
+                        
+                    num = get_number_substring_arrangements(sub_row, numbers_alt[:i])
+                    num_correct_combos += num * rem_options
+    else:
+        num_correct_combos = get_number_substring_arrangements(len(row), numbers_alt)
+
+    cache_dict[f'{str(row)} {str(numbers_alt)}'] = num_correct_combos
+    return num_correct_combos
+
+
 def get_num_combinations_for_row(row: str, numbers: list[int], cache_dict: dict = None) -> int:
     if cache_dict is None:
         cache_dict = {}
 
+    if row == "???" and numbers == [1]:
+        pass
+
     num_correct_combos = 0
-    if numbers == [] and row.count('#') == 0:
-        return 1
-    if row == '':
-        return 0
 
     first_row_spring_string = get_first_row_spring_string(row)
     spring_strings = [f"{'#'*number}." for number in numbers]
     if first_row_spring_string:
         for i, number in enumerate(numbers):
             for j in range(max(0, number - first_row_spring_string['length']) + 1):
-                if check_row_valid(row[first_row_spring_string['start'] - j: first_row_spring_string['start'] - j + numbers[i] + 1], spring_strings[i]):
+                if check_row_valid(row[first_row_spring_string['start'] - j: first_row_spring_string['start'] - j + numbers[i] + 1], spring_strings[i]): 
                     a_row = row[:first_row_spring_string['start'] - j]
                     a_nums = numbers[:i]
                     a = cache_dict.get(f'{str(a_row)} {str(a_nums)}', None)
@@ -92,23 +143,13 @@ def get_num_combinations_for_row(row: str, numbers: list[int], cache_dict: dict 
                     if b is None:
                         b = get_num_combinations_for_row(b_row, b_nums, cache_dict)
                     num_correct_combos += a * b
-                    a_nums.clear(), b_nums.clear()
                     
     else:
-        spring_strings = [f"{'#'*number}." for number in numbers]
-        num_spaces_to_fill = len(row) - len(''.join(spring_strings))
-        if num_spaces_to_fill and spring_strings:
-            possible_space_combinations = get_possible_space_combinations_validate(num_spaces_to_fill, len(spring_strings) + 1, row, spring_strings)
-            num_correct_combos = 0
-            for combination in possible_space_combinations:
-                experiment_row = ''.join(list(map(lambda x: x[0] + x[1], zip(combination, spring_strings)))) + combination[-1]
-                if check_row_valid(row, experiment_row):
-                    num_correct_combos += 1
-        else:
-            num_correct_combos = 1
-
+        num_correct_combos = cache_dict.get(f'{row} {numbers}')
+        if num_correct_combos is None: 
+            num_correct_combos = possible_no_hash_options(row, [number + 1 for number in numbers])
+        
     cache_dict[f'{str(row)} {str(numbers)}'] = num_correct_combos
-    #print(row, numbers, num_correct_combos)
     return num_correct_combos
 
 
@@ -116,15 +157,22 @@ def part_one(str_input: str):
     lines = str_input.split('\n')
     spring_rows = [(line.split()[0] + '.') for line in lines]
     spring_numbers = [[int(num) for num in line.split()[1].split(',')] for line in lines]
-    return sum(get_num_combinations_for_row(inputs[0],inputs[1]) for inputs in zip(spring_rows, spring_numbers))
+    sum = 0
+    for i, inputs in enumerate(zip(spring_rows, spring_numbers)):
+        num = get_num_combinations_for_row(inputs[0],inputs[1])
+        sum += num
+    return sum
     
 
-
 def test_part_one():
-    assert check_row_valid('??#?###', '#.#.###') == True
+    '''assert check_row_valid('??#?###', '#.#.###') == True
     assert check_row_valid('??#?###', '#...###') == False
     assert part_one('???.### 1,1,3') == 1
     assert part_one('?###???????? 3,2,1') == 10
+    assert possible_no_hash_options('??...', [2]) == 2
+    assert possible_no_hash_options('??...', [2,2]) == 0
+    assert possible_no_hash_options('.??..??...', [2,2]) == 4
+    assert possible_no_hash_options('???', [1]) == 3
     assert part_one('.??..??...?##. 1,1,3') == 4
     assert part_one('?#?#?#?#?#?#?#? 1,3,1,6') == 1
     assert part_one('?.???.???? 1,1,1') == 29
@@ -134,27 +182,25 @@ def test_part_one():
 ????.#...#... 4,1,1
 ????.######..#####. 1,6,5
 ?###???????? 3,2,1""") == 21
-    assert part_one('?#?#?#?#?#?#?#? 1,3,1,6') == 1
+    assert part_one('?#?#?#?#?#?#?#? 1,3,1,6') == 1'''
+    assert get_num_combinations_for_row('???', [1]) == 2
+    assert part_one('?????.?#.???#?#??? 2,1,2,1,4,1') == 12
+    assert part_one('.????.??#?#? 1,5') == 8
 
 
 # ----- PART TWO -----
 
 from datetime import datetime
 
+
 def part_two(str_input: str):
     lines = str_input.split('\n')
-    spring_rows = [((line.split()[0] + '?')*4 + line.split()[0] + '.') for line in lines]
+    spring_rows = [('?'.join(5*[line.split()[0]]) + '.') for line in lines]
     spring_numbers = [[int(num) for num in line.split()[1].split(',')]*5 for line in lines]
     sum = 0
     for i, inputs in enumerate(zip(spring_rows, spring_numbers)):
-        if inputs[0].count('#') == 0:
-            print(datetime.time(datetime.now()))
-            print(i, inputs[0],inputs[1])
-            sum += get_num_combinations_for_row(inputs[0],inputs[1])
-            print(datetime.time(datetime.now()))
-        else:
-            print(i)
-            sum += get_num_combinations_for_row(inputs[0],inputs[1])
+        print(i, lines[i])
+        sum += get_num_combinations_for_row(inputs[0],inputs[1])
     return sum
 
 
@@ -169,12 +215,35 @@ def test_part_two():
 ????.#...#... 4,1,1
 ????.######..#####. 1,6,5
 ?###???????? 3,2,1""") == 525152
+    assert part_two("""???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1
+???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1
+???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1""") == 3*525152
+    assert get_number_substring_arrangements(4, [2]) == 3
+    assert get_number_substring_arrangements(4, [1,1]) == 6
+    assert get_number_substring_arrangements(6, [2,1,1]) == 10
+    assert get_number_substring_arrangements(6, [1,1,1]) == 20
+    assert possible_no_hash_options('?.???.????.', [2,2,2]) == 29
 
 
 # --------------------
 
 
 if __name__ == "__main__":
-    print(part_one('?.???.???? 1,1,1'))
-    print(part_two('?.???.???? 1,1,1'))
-    #print(part_two(read_data("day_12.txt")))
+    part_one('?????.?#.???#?#??? 2,1,2,1,4,1')
+    print(part_one(read_data("day_12.txt")))
+    print(part_two(read_data("day_12.txt")))
---
+++ day_12_old.py
@@ -0,0 +1,118 @@
+
+from copy import deepcopy
+from itertools import product
+import pytest
+
+from read_data_function import read_data
+
+# ----- PART ONE -----
+
+
+def check_row_valid(row: str, experiment_row: str):
+    for i in range(len(row)):
+        if (row[i] != '?') and (row[i] != experiment_row[i]):
+            return False
+    return True
+
+
+def check_partial_combination(row: str, spring_strings: list[str], combination: list[int]):
+    experiment_row = ''.join([combination[i] + spring_strings[i] for i in range(len(combination))])
+    exp_row_length = len(experiment_row)
+    if not check_row_valid(row[:exp_row_length], experiment_row):
+        return False
+    remaining_row_hashes = row[exp_row_length:].count('#')
+    spring_lengths = [len(spring_string) - 1 for spring_string in spring_strings]
+    if (experiment_row.count('#') + remaining_row_hashes) > (sum(spring_lengths) + 1):
+        return False
+    if (remaining_row_hashes + row[exp_row_length:].count('?')) < (sum(spring_lengths[len(combination):]) + 1):
+        return False
+    return True
+
+
+def get_possible_space_combinations_validate(sum_value: int, num_spaces: int, row: str, spring_strings: list[str], prefix: list[int] = None):
+    if prefix is None:
+        prefix = []
+
+    if num_spaces == 1:
+        prefix.append(sum_value*'.')
+        return [prefix]
+
+    if prefix and not check_partial_combination(row, spring_strings, prefix):
+        return []
+
+    options = []
+    for i in range(sum_value + 1):
+        options += get_possible_space_combinations_validate(sum_value - i, num_spaces - 1, row, spring_strings, prefix + [i*'.'])
+    return options
+
+
+def get_num_combinations_for_row(row: str, numbers: list[int]) -> int:
+    spring_strings = [f"{'#'*number}." for number in numbers]
+    spring_strings[-1] = spring_strings[-1].rstrip('.')
+    num_spaces_to_fill = len(row) - len(''.join(spring_strings))
+    
+    possible_space_combinations = get_possible_space_combinations_validate(num_spaces_to_fill, len(spring_strings) + 1, row, spring_strings)
+    num_correct_combos = 0
+    for combination in possible_space_combinations:
+        experiment_row = ''.join(list(map(lambda x: x[0] + x[1], zip(combination, spring_strings)))) + combination[-1]
+        if check_row_valid(row, experiment_row):
+            num_correct_combos += 1
+    print(row, numbers, num_correct_combos)
+    return num_correct_combos
+
+
+def part_one(str_input: str):
+    lines = str_input.split('\n')[125:140]
+    spring_rows = [line.split()[0] for line in lines]
+    spring_numbers = [[int(num) for num in line.split()[1].split(',')] for line in lines]
+    return sum(get_num_combinations_for_row(inputs[0],inputs[1]) for inputs in zip(spring_rows, spring_numbers))
+
+
+
+def test_part_one():
+    assert check_row_valid('??#?###', '#.#.###') == True
+    assert check_row_valid('??#?###', '#...###') == False
+    assert get_num_combinations_for_row('???.###', [1,1,3]) == 1
+    assert get_num_combinations_for_row('?###????????', [3,2,1]) == 10
+    assert part_one("""???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1""") == 21
+    
+
+
+# ----- PART TWO -----
+
+
+def part_two(str_input: str):
+    lines = str_input.split('\n')
+    print(len(lines))
+    spring_rows = [((line.split()[0] + '?')*5)[:-1] for line in lines]
+    spring_numbers = [[int(num) for num in line.split()[1].split(',')]*5 for line in lines]
+    sum = 0
+    for i, inputs in enumerate(zip(spring_rows, spring_numbers)):
+        num_comb = get_num_combinations_for_row(inputs[0],inputs[1])
+        print(i, num_comb)
+        sum += num_comb
+    return sum
+
+
+def test_part_two():
+    assert part_two("""???.### 1,1,3""") == 1
+    assert part_two(".??..??...?##. 1,1,3") == 16384
+    assert part_two("?###???????? 3,2,1") == 506250
+    assert part_two("""???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1""") == 525152
+
+
+# --------------------
+
+
+if __name__ == "__main__":
+    print(part_one(read_data("day_12.txt")))
